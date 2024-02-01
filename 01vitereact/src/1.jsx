3005d445115f88e1be3bbd2d5713c328f3f941db
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-02-01 18:12:51 +0530

    React JS

---
+++ 01vitereact/src/1.jsx
@@ -0,0 +1,6 @@
+function Hello(){
+    return(
+        <h3>Hello world is ready</h3>
+    )
+}
+export default Hello
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./1"
+function App() {
+  
+
+  return (
+    <>
+    <Hello/>
+    <h1> Hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim inventore earum magni recusandae, mollitia eos vero accusantium fugiat quisquam repellat rem, quia ex.</p>
+    </>
+  )   
+  } 
+
+export default App
