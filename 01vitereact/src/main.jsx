b40533ad22bce1c6dcd21e5487e11f6f1e3b27b9
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-02-01 21:28:27 +0530

    React JS

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+function Myapp(){
+  return(
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <App/>
+    <Myapp />
   </React.StrictMode>
 );
